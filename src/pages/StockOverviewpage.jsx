0262370880030b766c08fb3778cafc14f0b1f932
Reena Yadav <yadav@1993reena.com>
2023-12-12 21:12:07 +0530

    fourth commit

--- src/pages/StockOverviewpage.jsx
+++ src/pages/StockOverviewpage.jsx
@@ -3,6 +3,9 @@ import StockList from "../components/StockList"
 
 export const StockOverviewPage = ()=>{
     return (<>
+    <div className="w-50 p-2 rounded mx-auto inputbox">
+        <img src="https://img.freepik.com/premium-vector/king-trading-logo-icon-vector-template_10060-450.jpg?w=2000" className="img"/>
+    </div>
     <AutoComplete/>
     <StockList/></>)
 }
